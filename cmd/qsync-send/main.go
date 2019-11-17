7c0d72133b489de673fb7d1fa85b2848a6794af4
Martin Holst Swende <martin@swende.se>
2019-11-17 14:49:57 +0100

    add cmdline options to qsync-send

--- cmd/qsync-send/main.go
+++ cmd/qsync-send/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -12,10 +14,32 @@ func init() {
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("Error: path not supplied")
+
+	disableCompression := flag.Bool("n", false, "`nocompress` disables compression")
+	verbosity := flag.Uint("v", 3, "`verbosity`: 0=None, 1=Error, 2=Warn, 3=Info, 4=Debug, 5=Trace")
+	ignoreSymlinks := flag.Bool("i", false, "`ignore-symlinks` - if set, symlinks are ignored")
+
+	opts := packer.DefaultOptions
+	if *disableCompression{
+		opts.Compression = packer.CompressionOff
 	}
-	sender, err := packer.NewSender(os.Stdout, os.Stdin, nil)
+	if *ignoreSymlinks{
+		opts.IgnoreSymlinks = true
+	}
+	opts.Verbosity = int(*verbosity)
+
+	flag.Parse()
+	flag.Usage = func(){
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage:\n %s [options] /directory/to/sync\nOptions:\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	if flag.NArg() < 1 {
+		fmt.Fprintf(flag.CommandLine.Output(), "Error: path not supplied\n")
+		flag.Usage()
+		os.Exit(1)
+	}
+	sender, err := packer.NewSender(os.Stdout, os.Stdin, opts)
 	if err != nil {
 		log.Fatal(err)
 	}
